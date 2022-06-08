6dbd9ce1b3cff7fb1809854f87ff19a88ed82bb2
Carlomagno José <66708949+Carlom4gn0@users.noreply.github.com>
2022-06-07 23:30:28 -0300

    Add files via upload
    upload files, project appointment react - veterinary

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: ["./index.html", "./src/**/*.{jsx}"],
+  darkMoe: false,
+  content: [],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend:[],
+  },
+  plugins: [],
+}
